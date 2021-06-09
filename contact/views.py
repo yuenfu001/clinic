4c3b5dbc79a13fb76976a738f6c38af623ae8d12
yuenfu001 <liuyuenfu@gmail.com>
2021-06-09 17:56:45 +0100

    added templates

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect,Http404
+from .models import *
 
 # Create your views here.
+def Home(request):
+    return render(request,'base/index.html')
+
+def Contact(request):
+    return render(request,"other/add.html")
