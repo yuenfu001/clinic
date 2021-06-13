5124609def983f9fa802a0de978f1e7d2df00b14
yuenfu001 <liuyuenfu@gmail.com>
2021-06-13 22:54:55 +0100

    allowed host

--- clinic/settings.py
+++ clinic/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-po2b)4(yrju(08$^1-mn091_$bgn$$#wkxu7v)1#3z-gxbuc7d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
